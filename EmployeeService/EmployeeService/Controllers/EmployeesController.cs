2bdc34245f65547e38da85f77f677030ba9568ec
Rafey <rafey.saleem@getcake.com>
2022-11-17 13:44:04 +0500

    Assesment Commited

---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<employee> Get()
+        {
+            using (employeeDBEntities entities = new employeeDBEntities())
+            {
+                return entities.employees.ToList();
+            }
+        }
+
+        public employee Get(int id)
+        {
+            using (employeeDBEntities entities = new employeeDBEntities())
+            {
+                return entities.employees.FirstOrDefault(e => e.emp_id == id);
+            }
+        }
+    }
+}
---
+++ EmployeeService/EmployeeService/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDataAccess.employeeDBEntities db = new EmployeeDataAccess.employeeDBEntities();
+        public ActionResult Index(string searching)
+        {
+            var result = db.employees.Where(x => x.emp_name.Contains(searching) || searching == null).ToList();
+            return View(result);
+        }
+    }
+}
